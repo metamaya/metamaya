0bd6730bb12d0a14bcb1b80dca163e0bc8b18e6a
Kalman Keri <kk@metamaya.org>
2016-08-06 13:53:19 +0200

    build: npm build script builds parser recipe

---
+++ build/build.js
@@ -0,0 +1,21 @@
+﻿"use strict"
+
+const fs = require("fs");
+const path = require("path");
+const ohm = require("ohm-js");
+
+buildParser();
+
+
+function buildParser() {
+	console.log("Building parser recipe...");
+
+	let parserPath = path.join(__dirname, '../ohm/parser.ohm');
+	let recipePath = path.join(__dirname, "../ohm/recipe.js");
+
+	let parser = ohm.grammar(fs.readFileSync(parserPath)/*, { Lexer: lexer }*/);
+	let recipe = parser.toRecipe();
+
+	fs.writeFileSync(recipePath, recipe, { encoding: "utf-8" });
+	console.log("Done.");
+}
