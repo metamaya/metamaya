ed3ccf852a5c5e177fd7e4b0da92b435cbb68361
Kalman Keri <kk@metamaya.org>
2016-11-01 14:39:21 +0100

    src: refactoring - renamed realm to context - renamed rewriteProperty() to rewrite() - introduced Closure[mm.Unreduce]() - added the - prefix operator to parser.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-﻿"use strict"
+﻿"use strict";
 
 const mm = require("../lib/metamaya");
 const test = require("tape");
@@ -50,6 +50,7 @@ test("function", (t) => {
 	t.equal(testStr('start = f(2); f(x) = x * x'), 4);
 	t.equal(testStr('start = f(2) + f(3); f(x) = x * x'), 13);
 	t.equal(testStr('start = f(f(3)); f(x) = x * x'), 81);
+	t.equal(testStr('start = f(g(3)); f(x) = x * x; g = x + 2'), 25);
 	t.end();
 });
 
